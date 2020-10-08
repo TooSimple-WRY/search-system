c163cef62a5e25ca4105403b0c569299c435e82a
TooSimple-WRY <1790454752@qq.com>
2020-10-08 21:10:31 +0800

    fit

--- search_engine/vocabulary_builder.py
+++
@@ -1,8 +0,0 @@
-passage = []
-def get_filename(text):
-    with open(text, encoding='utf-8') as fread:
-        for line in fread:
-            line = line.strip()  # 去掉两端的空白字符，如空格、回车等
-            passage.append(line)
-    fread.close()
-    return passage
